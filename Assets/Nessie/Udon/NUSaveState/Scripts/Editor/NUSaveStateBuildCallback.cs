ec1d4e76bad3fc18b3347b81d5c15aba39b21d9d
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-02-09 21:52:11 +0100

    AvatarSlots are now applied on Build.
    • Added a new class that implements the IVRCSDKBuildRequestedCallback interface in order to apply the AvatarSlots upon build.

---
+++ Assets/Nessie/Udon/NUSaveState/Scripts/Editor/NUSaveStateBuildCallback.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+namespace Nessie.Udon.SaveState.Internal
+{
+    public class NUSaveStateBuildCallback : IVRCSDKBuildRequestedCallback
+    {
+        public int callbackOrder => 0;
+
+        public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+        {
+            if (requestedBuildType != VRCSDKRequestedBuildType.Scene)
+                return true;
+
+            NUSaveState[] saveStates = Object.FindObjectsOfType<NUSaveState>();
+            foreach (NUSaveState saveState in saveStates)
+            {
+                if (saveState.TryGetComponent(out NUSaveStateData data))
+                {
+                    EditorUtility.SetDirty(saveState);
+                    data.ApplyAvatarSlots(saveState);
+                }
+            }
+            
+            return true;
+        }
+    }
+}
