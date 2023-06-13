3a9285a24230e21c90af911bb369c9d335b0ee5c
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-06-14 00:41:07 +0200

    Added a class for constants.
    • Added a class containing data constants such that they can be more easily used throughout the project.

---
+++ Assets/Nessie/Udon/NUSaveState/Scripts/U#/DataConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Nessie.Udon.SaveState.Data
+{
+    public class DataConstants
+    {
+        public const int BITS_PER_PAGE = 16 * 16; // Bits per bone * bone count.
+        public const int BYTES_PER_PAGE = BITS_PER_PAGE / 8;
+        public const int MAX_PAGE_COUNT = 256; // 1 byte for page index.
+        public const int MAX_BIT_COUNT = BITS_PER_PAGE * MAX_PAGE_COUNT;
+        public const string DEFAULT_PARAMETER_NAME = "parameter";
+    }
+}
