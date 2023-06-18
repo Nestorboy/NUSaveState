87bb1a51c09076ceeb074b7c5de0e0e2378490f7
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2023-06-18 04:05:00 +0200

    Removed comment in DataConstants.
    • Removed residual comment in DataConstants that only adds confusion.

--- Assets/Nessie/Udon/NUSaveState/Scripts/U#/DataConstants.cs
+++ Assets/Nessie/Udon/NUSaveState/Scripts/U#/DataConstants.cs
@@ -4,7 +4,7 @@
     {
         public const int BITS_PER_PAGE = 16 * 16; // Bits per bone * bone count.
         public const int BYTES_PER_PAGE = BITS_PER_PAGE / 8;
-        public const int MAX_PAGE_COUNT = 256; // 1 byte for page index.
+        public const int MAX_PAGE_COUNT = 256;
         public const int MAX_BIT_COUNT = BITS_PER_PAGE * MAX_PAGE_COUNT;
         public const string DEFAULT_PARAMETER_NAME = "parameter";
     }
